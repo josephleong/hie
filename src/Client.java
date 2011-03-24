b96eb07f6f8cc2a0775ab8ff0126dfe628d00640
Joseph Leong <joseph.k.leong@gmail.com>
2011-03-23 20:45:09 -0500

    encryption working

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
-import java.io.*;
 
 public
 class Client {
--- src/Server.java
+++ src/Server.java
@@ -1,22 +1,51 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.util.ArrayList;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class Server {
-	public static void main(String[] args) {
+	
+	private static byte[] keyBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+	        0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+	private static byte[] ivBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x03, 0x00, 0x00, 0x00,
+	        0x00, 0x00, 0x00, 0x00, 0x01 };
+	
+	
+	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException, IOException {
+				
 		try {
 			SSLSocket sslsocket = handshake();
 			
 			InputStream inputstream = sslsocket.getInputStream();
 			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
 			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-			
+			ArrayList<byte[]> test = new ArrayList<byte[]>();			
 			String string = null;
 			while ((string = bufferedreader.readLine()) != null) {
-				System.out.println(string);
+				byte[] b = (encrypt(string));
+				test.add(b); 
+				System.out.println(b);
+				if(test.size() % 5 == 0)
+				for(byte[] s: test)
+					System.out.println(decrypt(s));
 				System.out.flush();
 			}
 		
@@ -44,6 +73,47 @@ public class Server {
 			exception.printStackTrace();
 			return null;
 		}
+	}
+	
+	private static byte[] encrypt(String s) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());        
+	    byte[] input = s.getBytes();
+	    
+	    
+	    SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+	    IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+	    Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC");
+	    System.out.println("input : " + new String(input));
+
+	    // encryption pass
+	    cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+	    ByteArrayInputStream bIn = new ByteArrayInputStream(input);
+	    CipherInputStream cIn = new CipherInputStream(bIn, cipher);
+	    ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+	    int ch;
+	    while ((ch = cIn.read()) >= 0) {
+	      bOut.write(ch);
+	    }
+
+	    byte[] cipherText = bOut.toByteArray();
+
+	    return  (cipherText);
+
+	    
+	}
+	
+	private static String decrypt(byte[] s) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException {
+		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC");
+		// decryption pass
+		SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+	    IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+	    cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+	    ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+	    CipherOutputStream cOut = new CipherOutputStream(bOut, cipher);
+	    cOut.write(s);
+	    cOut.close();
+	    return new String(bOut.toByteArray());
 	}
 }
 
