3f33d087a71e5d8138d2f1eb6c33adfd0283335c
Joseph Leong <joseph.k.leong@gmail.com>
2011-05-02 12:41:23 -0500

    cleaned up old files in old and working dir's

--- old/ReadRecord.java
+++
@@ -1,9 +0,0 @@
-
-public class ReadRecord extends Request {
-
-	public ReadRecord(long uid) {
-		super(uid);
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- old/Response.java
+++
@@ -1,6 +0,0 @@
-import java.io.Serializable;
-
-
-public class Response implements Serializable {
-	
-}
